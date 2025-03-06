16158b9b0281f77804dda71ddb4437cc098077f9
Mahammadali12 <zamanli.mehemmedeli@gmail.com>
2025-03-06 17:39:05 -0500

    Handle case where L1 and L2 are equal:

--- Simulation.java
+++ Simulation.java
@@ -15,16 +15,16 @@ public class Simulation {
     Queue<Integer> Q;
     
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Simulation simulation = new Simulation();
         simulation.runSimulation();
     }
 
-    public void  runSimulation(){
+    public void  runSimulation() throws InterruptedException{
         random = new Random();
         Q = new PriorityQueue<>();
-        L1 = random.nextInt(20);
-        L2 = random.nextInt(30);
+        L1 = random.nextInt(50);
+        L2 = random.nextInt(50);
 
         H = 501;
         System.out.println("---------------------------------------------------------------");
@@ -37,10 +37,10 @@ public class Simulation {
                 
                 if(L1 < L2 && L1 < H){
                     T = L1;
-                    L1 = T + random.nextInt(20);
+                    L1 = T + random.nextInt(50);
 
                     if(serverIsAvailable){
-                        H = T + random.nextInt(10);
+                        H = T + random.nextInt(50);
                         printTable("DALDAN");
                         serverIsAvailable = false;
                     }else{
@@ -49,10 +49,10 @@ public class Simulation {
                 }
                 else if (L2 < L1 && L2 < H) {
                     T = L2;
-                    L2 = T + random.nextInt(30);
+                    L2 = T + random.nextInt(50);
 
                     if(serverIsAvailable){
-                        H = T + random.nextInt(15);
+                        H = T + random.nextInt(50);
                         printTable("DALDAN");
                         serverIsAvailable = false;
                     }else{
@@ -65,19 +65,28 @@ public class Simulation {
                     H = 501;
 
                     if(Q.isEmpty()){
+                        System.out.println("asdasds");
                         continue;
                     }else{
                         int temp = Q.poll();
                         if(temp == L1){
-                            H = T + random.nextInt(10);
+                            H = T + random.nextInt(50);
                             printTable("DALDAN");
                             serverIsAvailable = false;
                         }else if (temp == L2){
-                            H = T + random.nextInt(15);
+                            H = T + random.nextInt(55);
                             serverIsAvailable = false;
                             printTable("DALDAN");
                         }
                     }
+                }else{
+                    try {
+                        Thread.sleep(1000);
+                        
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    System.out.println("SICDIN");
                 }
             }
         }
